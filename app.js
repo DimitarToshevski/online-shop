110a4e431522bab0a7e9315fef1e26dbcb435f74
Dimitar Toshevski <dimitar.toshevski@mentormate.com>
2020-08-15 18:15:20 +0300

    Simple redirect

--- app.js
+++ app.js
@@ -1,7 +1,30 @@
 const http = require('http');
+const fs = require('fs');
 
 const server = http.createServer((req, res) => {
-  console.log(req);
+  const url = req.url;
+  const method = req.method;
+
+  if (url === '/') {
+    res.write('<html>');
+    res.write('<head><title>Enter message');
+    res.write('</head></title>');
+    res.write(
+      '<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form>'
+    );
+    res.write('</body>');
+    res.write('</html>');
+    return res.end();
+  }
+
+  if (url === '/message' && method === 'POST') {
+    fs.writeFileSync('message.txt', 'DUMMY');
+    res.statusCode = 302;
+    res.setHeader('Location', '/');
+    return res.end();
+  }
+
+  res.setHeader('Content-Type', 'text/html');
 });
 
 server.listen(3000);
