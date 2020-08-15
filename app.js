1623cb84859593fcd379317dd31c16a9a49c650e
Dimitar Toshevski <dimitar.toshevski@mentormate.com>
2020-08-15 18:40:31 +0300

    Callbacks

--- app.js
+++ app.js
@@ -22,15 +22,15 @@ const server = http.createServer((req, res) => {
     req.on('data', (chunk) => {
       body.push(chunk);
     });
-    req.on('end', () => {
+    return req.on('end', () => {
       const parsedBody = Buffer.concat(body).toString();
       const message = parsedBody.split('=')[1];
-      fs.writeFileSync('message.txt', message);
+      fs.writeFile('message.txt', message, (err) => {
+        res.statusCode = 302;
+        res.setHeader('Location', '/');
+        return res.end();
+      });
     });
-
-    res.statusCode = 302;
-    res.setHeader('Location', '/');
-    return res.end();
   }
 
   res.setHeader('Content-Type', 'text/html');
