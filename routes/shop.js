88ee4779041097276ff1356af7dad3cd1e3eb6d5
Dimitar Toshevski <dimitar.toshevski@mentormate.com>
2020-08-15 21:48:44 +0300

    Add templates for the shop and the add products

--- routes/shop.js
+++ routes/shop.js
@@ -8,7 +8,9 @@ const adminData = require('./admin');
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
-  res.render('shop');
+  const products = adminData.products;
+
+  res.render('shop', { products, pageTitle: 'Shop' });
 });
 
 module.exports = router;
