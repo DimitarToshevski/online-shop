3e0e032d4b92fa9f5450ded70c7188175724e513
Dimitar Toshevski <dimitar.toshevski@mentormate.com>
2020-08-22 13:55:14 +0300

    Add delete cart items functionality

--- controllers/shop.js
+++ controllers/shop.js
@@ -94,10 +94,22 @@ const postCart = (req, res, next) => {
 
 const postCartDeleteProduct = (req, res, next) => {
   const productId = req.body.productId;
-  Product.findById(productId, (product) => {
-    Cart.deleteProduct(productId, product.price);
-    res.redirect('/cart');
-  });
+  req.user
+    .getCart()
+    .then((cart) => {
+      return cart.getProducts({ where: { id: productId } });
+    })
+    .then((products) => {
+      const product = products[0];
+
+      return product.cartItem.destroy();
+    })
+    .then(() => {
+      res.redirect('/cart');
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 };
 
 const getOrders = (req, res, next) => {
