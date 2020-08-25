a572d99af8d25cc1d41f385e0579cd6a3be80b59
Dimitar Toshevski <dimitar.toshevski@mentormate.com>
2020-08-25 23:59:34 +0300

    Add some configuration

--- app.js
+++ app.js
@@ -170,8 +170,8 @@ sequelize
     });
   })
   .then(() => {
-    console.log('Listening on port: ', 3000);
+    console.log('Listening on port: ', process.env.PORT || 3000);
 
-    app.listen(3000);
+    app.listen(process.env.PORT || 3000);
   })
   .catch((err) => console.log(err));
